9bf35b3ccdfe6bcf2a2930e4c97a0ae35db1484c
Alex Henderson <bohroco@gmail.com>
2020-03-21 14:54:04 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,27 +3,28 @@ function $(x) { return document.getElementById(x); }
 function getDefaultUser() {
 	return {
 		totPower:new Decimal(0),
-		blue: {
+		red: {
 			tick:0,
 			tickMax:new Decimal(1000),
 			tickMultPrice:new Decimal(1.00e4),
 			tickMultCount:new Decimal(0),
 			mults: [new Decimal(1)],
-			limits: [new Decimal(10)],
 			buttonPrice: [new Decimal(10)],
+			addButtonPrice: new Decimal(100),
+			index: new Decimal(1),
+			//All the above gets reset on a red reset
+			limits: [new Decimal(10)],
 			breakPrice: [new Decimal(3)],
 			clicked: 0,
 			upgrades:       ["PB","CP","LB","BB","CPB","RB"],
 			upgradeCount:   [new Decimal(0)   ,new Decimal(0)   ,new Decimal(0)   ,new Decimal(0)   ,new Decimal(0)   ,new Decimal(0)],
 			upgradePrices:  [new Decimal(1)   ,new Decimal(1)   ,new Decimal(10)  ,new Decimal(50)  ,new Decimal(100) ,new Decimal(10)],
 			upgradeIncrease:[new Decimal(0)   ,new Decimal(0)   ,new Decimal(50)  ,new Decimal(50)  ,new Decimal(1e3) ,new Decimal(0)],
-			addButtonPrice: new Decimal(100),
-			index: new Decimal(1),
 			indexLimit: new Decimal(10),
 			energy: new Decimal(0),
 			resets: new Decimal(0),
 		},
-		green: {
+		orange: {
 			tick:new Decimal(0),
 			tickMax:new Decimal(1000),
 			tickMultPrice:new Decimal(1e4),
@@ -48,67 +49,67 @@ function update(get, set) {
 function gameCycle(){
 	let now = new Date().getTime();
 	let diff = now - user.lastTick;
-	let tickMax = user.blue.tickMax.times(Decimal.pow(0.9,user.blue.tickMultCount));
-	user.blue.tick += diff;
-	if(user.blue.tick >= user.blue.tickMax) process(Decimal.round(new Decimal(user.blue.tick).div(user.blue.tickMax)));
-	update("blueCycle", `Reset Cycle: ${user.blue.tick}/${user.blue.tickMax}`);
+	let tickMax = user.red.tickMax.times(Decimal.pow(0.9,user.red.tickMultCount));
+	user.red.tick += diff;
+	if(user.red.tick >= user.red.tickMax) process(Decimal.round(new Decimal(user.red.tick).div(user.red.tickMax)));
+	update("redCycle", `Reset Cycle: ${user.red.tick}/${user.red.tickMax}`);
 	user.lastTick = now;
 	updateAll();
 }
 
-function blueClick(num) {
-	let mid=user.blue.upgradeCount[1];
-	let old=user.blue.clicked;
-	user.blue.clicked=num;
+function redClick(num) {
+	let mid=user.red.upgradeCount[1];
+	let old=user.red.clicked;
+	user.red.clicked=num;
 	if(mid.gt(new Decimal(0))){
 		if(num!==old) {
-			user.blue.mults[num-1]=new Decimal(""+mid+user.blue.mults[num-1]);
+			user.red.mults[num-1]=new Decimal(""+mid+user.red.mults[num-1]);
 			if(old!==0){
-				user.blue.mults[old-1]=new Decimal(user.blue.mults[old-1].toString().substring(1));
+				user.red.mults[old-1]=new Decimal(user.red.mults[old-1].toString().substring(1));
 			}
 		}
 	}
 }
 
-function getBlueButtonTotalMult() {
+function getRedButtonTotalMult() {
 	var mult=new Decimal(1);
-	user.blue.mults.forEach(function(value) {
+	user.red.mults.forEach(function(value) {
 		mult = mult.times(value)
 	});
-	if(user.blue.upgradeCount[0].gt(0)){ 
-		mult = mult.times(user.blue.energy.plus(1));
-		update("currentPBBonus",display(user.blue.energy.plus(1)));
+	if(user.red.upgradeCount[0].gt(0)){ 
+		mult = mult.times(user.red.energy.plus(1));
+		update("currentPBBonus",display(user.red.energy.plus(1)));
 	}
-	if(user.blue.upgradeCount[5].gt(0)){
-		mult = mult.times(user.blue.resets.div(5).plus(1));
-		update("currentRBBonus",display(user.blue.resets.div(5).plus(1)));
+	if(user.red.upgradeCount[5].gt(0)){
+		mult = mult.times(user.red.resets.div(5).plus(1));
+		update("currentRBBonus",display(user.red.resets.div(5).plus(1)));
 	}
 	return mult;
 }
 
 function process(num) {
-	let tot = getBlueButtonTotalMult().times(num)
+	let tot = getRedButtonTotalMult().times(num)
 	user.totPower = user.totPower.plus(tot)
-	user.blue.tick=0;
+	user.red.tick=0;
 }
 
-function blueCycleUpg() {
-	var price=user.blue.tickMultPrice;
+function redCycleUpg() {
+	var price=user.red.tickMultPrice;
 	if(user.totPower.gte(price)) {
 		user.totPower = user.totPower.minus(price);
-		user.blue.tickMax = display(user.blue.tickMax.times(0.9));
-		user.blue.tickMultPrice = user.blue.tickMultPrice.times(100);
-		user.blue.tickMultCount = user.blue.tickMultCount.plus(1);
-		update("blueCycleUpgCost", display(user.blue.tickMultPrice));
+		user.red.tickMax = display(user.red.tickMax.times(0.9));
+		user.red.tickMultPrice = user.red.tickMultPrice.times(100);
+		user.red.tickMultCount = user.red.tickMultCount.plus(1);
+		update("redCycleUpgCost", display(user.red.tickMultPrice));
 	}
 }
 	
 function checkButtonUpgrade(num) {
-	var price=user.blue.buttonPrice[num-1];
-	if(user.totPower.gte(price)&&user.blue.limits[num-1].gt(user.blue.mults[num-1])) {
+	var price=user.red.buttonPrice[num-1];
+	if(user.totPower.gte(price)&&user.red.limits[num-1].gt(user.red.mults[num-1])) {
 		user.totPower = user.totPower.minus(price);
-		user.blue.mults[num-1] = user.blue.mults[num-1].plus(new Decimal(1));
-		user.blue.buttonPrice[num-1] = price.times(new Decimal(2.5));
+		user.red.mults[num-1] = user.red.mults[num-1].plus(new Decimal(1));
+		user.red.buttonPrice[num-1] = price.times(new Decimal(2.5));
 	}
 	updateAll();
 }
@@ -131,46 +132,46 @@ function canBuyUpgrade(color, index) {
 	return user[color].energy.gte(user[color].upgradePrices[index])
 }
 
-function checkAddBlue() {
-	if(user.blue.index.lt(user.blue.indexLimit)){
-		if(user.totPower.gte(user.blue.addButtonPrice)){
-			user.totPower = user.totPower.minus(user.blue.addButtonPrice)
-			user.blue.index = user.blue.index.plus(1);
-			if(user.blue.index.gt(10)){
-				addBlueButton(user.blue.index);
+function checkAddRed() {
+	if(user.red.index.lt(user.red.indexLimit)){
+		if(user.totPower.gte(user.red.addButtonPrice)){
+			user.totPower = user.totPower.minus(user.red.addButtonPrice)
+			user.red.index = user.red.index.plus(1);
+			if(user.red.index.gt(10)){
+				addRedButton(user.red.index);
 			}
-			else if(user.blue.index.lte(10)){
-				$("buttonSet"+user.blue.index).style.display="block";
+			else if(user.red.index.lte(10)){
+				$("buttonSet"+user.red.index).style.display="block";
 			}
-			console.log(user.blue.index);
-			user.blue.mults.push(new Decimal(2));
-			user.blue.limits.push(new Decimal(10));
+			console.log(user.red.index);
+			user.red.mults.push(new Decimal(2));
+			user.red.limits.push(new Decimal(10));
 			let j = index-1;
-			user.blue.breakPrice.push(Decimal.floor(user.blue.breakPrice[j].times(1.5)));
-			user.blue.buttonPrice.push(Decimal.pow(new Decimal(10),user.blue.index).times(new Decimal(2.5)));
-			user.blue.addButtonPrice = user.blue.addButtonPrice.times(10);
+			user.red.breakPrice.push(Decimal.floor(user.red.breakPrice[j].times(1.5)));
+			user.red.buttonPrice.push(Decimal.pow(new Decimal(10),user.red.index).times(new Decimal(2.5)));
+			user.red.addButtonPrice = user.red.addButtonPrice.times(10);
 		}
 	}
 	updateAll();
 }
 
-function addBlueButton(n) {
+function addRedButton(n) {
 	window["buttonSet"+n] = document.createElement('div');
-	window["blueCircle"+n] = document.createElement('button');
+	window["redCircle"+n] = document.createElement('button');
 	window["upgrade"+n] = document.createElement('button');
 	window["break"+n] = document.createElement('button');
 	window["buttonSet"+n].class = 'lowerLayer';
 	window["buttonSet"+n].style = 'display:block';
-	window["blueCircle"+n].onclick = 'blueClick("+n+")';
-	window["blueCircle"+n].class = 'blueButtonSmall';
-	window["blueCircle"+n].innerHTML = 'x1';
+	window["redCircle"+n].onclick = 'redClick("+n+")';
+	window["redCircle"+n].class = 'redButtonSmall';
+	window["redCircle"+n].innerHTML = 'x1';
 	window["upgrade"+n].onclick = 'checkButtonUpgrade("+n+")';
-	window["upgrade"+n].class = 'blueButtonSmall';
-	window["upgrade"+n].innerHTML = 'Upgrade your Blue Button<br/>Cost: 100 Power';
+	window["upgrade"+n].class = 'redButtonSmall';
+	window["upgrade"+n].innerHTML = 'Upgrade your Red Button<br/>Cost: 100 Power';
 	window["break"+n].onclick = 'breakUpgrade("+n+")';
 	window["break"+n].class = 'breakLimitButton';
-	window["break"+n].innerHTML = "LIMIT BREAK!<br/>Cost: 5 <span style='color:darkBlue'>Energy</span>"
-	$("buttonSet"+n).appendChild($("blueCircle"+n));
+	window["break"+n].innerHTML = "LIMIT BREAK!<br/>Cost: 5 <span style='color:darkRed'>Energy</span>"
+	$("buttonSet"+n).appendChild($("redCircle"+n));
 	$("buttonSet"+n).appendChild($("upgrade"+n));
 	$("buttonSet"+n).appendChild($("break"+n));
 	$("buttonArea").appendChild($("buttonSet"+n));
@@ -178,33 +179,34 @@ function addBlueButton(n) {
 
 function breakUpgrade(num) {
 	let j = num-1;
-	if(user.blue.energy.gte(user.blue.breakPrice[j])) {
-		user.blue.energy = user.blue.energy.minus(user.blue.breakPrice[j]);
-		user.blue.limits[j] = user.blue.limits[j].times(10);
+	if(user.red.energy.gte(user.red.breakPrice[j])) {
+		user.red.energy = user.red.energy.minus(user.red.breakPrice[j]);
+		user.red.limits[j] = user.red.limits[j].times(10);
 		let name = "break"+j;
 		$(name).style.display = "none";
-		user.blue.breakPrice[j] = user.blue.breakPrice[j].times(25);
+		user.red.breakPrice[j] = user.red.breakPrice[j].times(25);
 	}
 }
 
-function getBluePrestige() {
+function getRedPrestige() {
 	if (user.totPower.gte(new Decimal(1e10))) return user.totPower.log10().minus(9).log2().plus(1);
 	else return new Decimal(0);
 }
 
-function blueReset() {
-	if(getBluePrestige().gte(1)){
-		let energy = user.blue.energy.plus(getBluePrestige());
-		let count = user.blue.upgradeCount;
-		let prices = user.blue.upgradePrices;
-		let resets = user.blue.resets.plus(1);
-		user.blue = getDefaultUser().blue;
+function redReset() {
+	if(getRedPrestige().gte(1)){
+		luser.red.energy = user.red.energy.plus(getRedPrestige());
+		user.red.resets = user.red.resets.plus(1);
+		user.red.tick = getDefaultUser().red.tick;
+		user.red.tickMax = getDefaultUser().red.tickMax;
+		user.red.tickMaxPrice = getDefaultUser().red.tickMaxPrice;
+		user.red.tickMultCount = getDefaultUser().red.tickMultCount;
+		user.red.mults = getDefaultUser().red.mults;
+		user.red.buttonPrice = getDefaultUser().red.buttonPrice;
+		user.red.addButtonPrice = getDefaultUser().red.addButtonPrice;
+		user.red.index = getDefaultUser().red.index;
 		user.totPower = new Decimal(0);
-		user.blue.energy = energy;
-		user.blue.upgradeCount = count;
-		user.blue.upgradePrices = prices;
-		user.blue.resets = resets;
-		update("blueCycleUpgCost", new Decimal(1e4));
+		update("redCycleUpgCost", new Decimal(1e4));
 		updateAll();
 	}
 }
@@ -256,47 +258,47 @@ function importSave() {
 function updateAll(){
 	update("powerAmount", "Total Power: "+display(user.totPower));
 	if(user.totPower.lt(1e4)){
-		$("blueCycleReduc").style.display = "none";
+		$("redCycleReduc").style.display = "none";
 	}
 	if(user.totPower.gte(1e4)){
-		$("blueCycleReduc").style.display = "";
+		$("redCycleReduc").style.display = "";
 	}
-	if(user.blue.energy.gt(0)){
-		$("blueEnergyArea").style.display = "";
-		$("blueEnergyAmount").innerHTML = display(user.blue.energy);
+	if(user.red.energy.gt(0)){
+		$("redEnergyArea").style.display = "";
+		$("redEnergyAmount").innerHTML = display(user.red.energy);
 		$("tabs").style.display = "";
 	}
-	else { $("blueEnergyArea").style.display = "none";}
-	var dispMult = display(getBlueButtonTotalMult());
+	else { $("redEnergyArea").style.display = "none";}
+	var dispMult = display(getRedButtonTotalMult());
 	update("powerMultArea", "Button Mult: x"+dispMult);
-	update("blueCycle", `Reset Cycle: ${user.blue.tick}/${user.blue.tickMax}`);
-	for(var i=1;i<user.blue.mults.length+1;i++){
+	update("redCycle", `Reset Cycle: ${user.red.tick}/${user.red.tickMax}`);
+	for(var i=1;i<user.red.mults.length+1;i++){
 		$("buttonSet"+i).style.display="block";
-		var name = "blueCircle" + i;
-		update(name, "x"+display(user.blue.mults[i-1]));
+		var name = "redCircle" + i;
+		update(name, "x"+display(user.red.mults[i-1]));
 		let bLButtons = document.getElementsByClassName("breakLimitButton");
 		var bLButton;
 		for (var j = 0; j < bLButtons.length; j++) {
 			bLButton = bLButtons.item(j);
-			if (user.blue.upgradeCount[2]>0) {
+			if (user.red.upgradeCount[2]>0) {
 				bLButton.style.display = 'block';
 			}
 			else bLButton.style.display = 'none';
 		}
-		if (user.blue.limits[i-1].gt(user.blue.mults[i-1])) {
-			update("upgrade"+i, `Upgrade your Blue button<br/>Cost: ${display(user.blue.buttonPrice[i-1])} Power`);
+		if (user.red.limits[i-1].gt(user.red.mults[i-1])) {
+			update("upgrade"+i, `Upgrade your Red button<br/>Cost: ${display(user.red.buttonPrice[i-1])} Power`);
 		} else {
 			update("upgrade"+i, "Max Multiplier!");
 		}	
 	}
-	let max = user.blue.indexLimit;
-	for(var i=user.blue.mults.length+1;i<=max;i++){
+	let max = user.red.indexLimit;
+	for(var i=user.red.mults.length+1;i<=max;i++){
 		$("buttonSet"+i).style.display="none";
 	}
-	var dispAddBluePrice = display(user.blue.addButtonPrice);
-	update("addBlueButton", `Add another Blue Button<br/>Cost: ${dispAddBluePrice} Power`);
-	for(i=0;i<user.blue.buttonPrice.length;i++){
-		if(user.totPower.gte(user.blue.buttonPrice[i])||user.blue.mults[i].eq(user.blue.limits[1])) {
+	var dispAddRedPrice = display(user.red.addButtonPrice);
+	update("addRedButton", `Add another Red Button<br/>Cost: ${dispAddRedPrice} Power`);
+	for(i=0;i<user.red.buttonPrice.length;i++){
+		if(user.totPower.gte(user.red.buttonPrice[i])||user.red.mults[i].eq(user.red.limits[1])) {
 			var j = i+1;
 			$("upgrade"+j).style.opacity = 1.0;
 		}
@@ -304,41 +306,41 @@ function updateAll(){
 			var j = i+1;
 			$("upgrade"+j).style.opacity = 0.6;
 		}
-		if(user.blue.upgradeCount[2].gt(0)){
+		if(user.red.upgradeCount[2].gt(0)){
 			$("break"+j).style.display = "";
 		}
 	}
-	if(user.totPower.gte(user.blue.addButtonPrice)) $("addBlueButton").style.opacity = 1.0;
-	else $("addBlueButton").style.opacity = 0.6;
-	if(user.blue.index.gte(user.blue.indexLimit)) {
-		$("addBlueButton").style.display = "none";
+	if(user.totPower.gte(user.red.addButtonPrice)) $("addRedButton").style.opacity = 1.0;
+	else $("addRedButton").style.opacity = 0.6;
+	if(user.red.index.gte(user.red.indexLimit)) {
+		$("addRedButton").style.display = "none";
 	} else {
-		$("addBlueButton").style.display = "";
+		$("addRedButton").style.display = "";
 	}
 	if(user.totPower.gte(1e10)){
-		$("bluePrestigeButton").style.display = "";
+		$("redPrestigeButton").style.display = "";
 	} else {		
-		$("bluePrestigeButton").style.display = "none";
+		$("redPrestigeButton").style.display = "none";
 	}
-	if(user.totPower.gte(user.blue.tickMultPrice)) $("blueCycleReduc").style.opacity = 1.0;
-	else $("blueCycleReduc").style.opacity = 0.6;
-	$("bluePrestigeButton").style.opacity = getBluePrestige().gt(0)?1:0.6
-	$("bluePrestigeAmount").innerHTML = display(getBluePrestige()) + " Energy";
-	$("currentCPBBonus").innerHTML = user.blue.upgradeCount[5];
-	user.blue.upgrades.forEach(function(id){
-		let i = user.blue.upgrades.indexOf(id);
-		if(user.blue.upgradeCount[i].gt(0)&&user.blue.upgradeIncrease[i].lte(0)){
+	if(user.totPower.gte(user.red.tickMultPrice)) $("redCycleReduc").style.opacity = 1.0;
+	else $("redCycleReduc").style.opacity = 0.6;
+	$("redPrestigeButton").style.opacity = getRedPrestige().gt(0)?1:0.6
+	$("redPrestigeAmount").innerHTML = display(getRedPrestige()) + " Energy";
+	$("currentCPBBonus").innerHTML = user.red.upgradeCount[5];
+	user.red.upgrades.forEach(function(id){
+		let i = user.red.upgrades.indexOf(id);
+		if(user.red.upgradeCount[i].gt(0)&&user.red.upgradeIncrease[i].lte(0)){
 			$(id).style.background = "forestGreen";
 		}
-		if(user.blue.energy.gte(user.blue.upgradePrices[i])){
+		if(user.red.energy.gte(user.red.upgradePrices[i])){
 			$(id).style.opacity = 1.0;
 		}
-		else if(user.blue.energy.lt(user.blue.upgradePrices[i])){
+		else if(user.red.energy.lt(user.red.upgradePrices[i])){
 			$(id).style.opacity = 0.6;
 		}
 	});
 	let ten = new Decimal(10);
-	user.blue.indexLimit = ten.plus(user.blue.upgradeCount[3].times(10));
+	user.red.indexLimit = ten.plus(user.red.upgradeCount[3].times(10));
 	showTab(user.currentTab);
 }
 
