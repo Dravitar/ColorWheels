e9bc7bf1948fe02a24b95ce880d7c529619cebf8
Alex Henderson <bohroco@gmail.com>
2020-03-28 18:01:52 -0400

    Update game.js

--- game.js
+++ game.js
@@ -84,14 +84,21 @@ function redClick(num) {
 function getRedButtonTotalMult() {
 	var mult=new Decimal(1);
 	var index=0;
-	user.red.mults.forEach(function(value) {
+	for(i=0;i<user.red.mults.length;i++){
+		let x = new Decimal(user.red.mults[i]);
+		if x.equals(user.red.limits[i]){
+			x = x.pow(new Decimal(1).plus(user.red.upgradeCount[8].div(10)));
+		}
+		mult = mult.times(x);
+	}
+	/*user.red.mults.forEach(function(value) {
 		let x = new Decimal(value);
 		if(x.equals(user.red.limits[index])){
 			x = x.pow(new Decimal(1).plus(user.red.upgradeCount[8].div(10)));
 		}
 		mult = mult.times(x);
 		index++;
-	});
+	});*/
 	if(user.red.upgradeCount[0].gt(0)){ 
 		mult = mult.times(user.red.energy.plus(1));
 		update("currentPBBonus",display(user.red.energy.plus(1)));
